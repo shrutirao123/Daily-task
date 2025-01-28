ac9ce11f58aae0b8ff530ad2957775c961facad5
shrutirao123 <95671259+shrutirao123@users.noreply.github.com>
2025-01-28 17:20:26 +0530

    Delete DAY2 directory

--- DAY2/Prgm1.py
+++
@@ -1,8 +0,0 @@
-# Accepting input from the user
-number = int(input("Enter a number: "))
-
-# Using ternary operator to check if the number is even or odd
-result = "Even" if number % 2 == 0 else "Odd"
-
-# Printing the result
-print("The number is:",result)
