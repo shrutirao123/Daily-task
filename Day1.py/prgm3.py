5f00ffa8ce260eb5215a1bed04269b1a1b4c98bf
shrutirao123 <95671259+shrutirao123@users.noreply.github.com>
2025-01-28 17:20:56 +0530

    Delete Day1.py directory

--- Day1.py/prgm3.py
+++
@@ -1,8 +0,0 @@
-#Accepting inputs from users 
-celsius = int(input("Enter temperature in Celsius: "))
-
-## Converting the temperature from Celsius to Fahrenheit using the formula
-fahrenheit = (celsius * 9 / 5) + 32
-
-# Display the largest number
-print("Temperature in Fahrenheit:", fahrenheit)
